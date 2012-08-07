5e6b9bf682d23feec2d1ee75ce1dc3d9cbbf0f62
yamagu <www.yamagu@gmail.com>
2012-08-07 10:32:57 +0000

    first commit

---
+++ screen/attributes/default.rb
@@ -0,0 +1,2 @@
+
+default[:users] = ["root", "yamaguchi"]
---
+++ screen/recipes/default.rb
@@ -0,0 +1,36 @@
+#
+# Cookbook Name:: screen
+# Recipe:: default
+#
+# Copyright 2012, Example Com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+package "screen" do
+    action :install
+end
+
+require 'etc'
+node[:users].each do |user|
+  pw = Etc.getpwnam(user)
+  p pw
+  rcfile = "#{pw.dir}/.screenrc"
+
+  template rcfile do
+    source "screenrc.erb"
+    owner pw.name
+    group Etc.getgrgid(pw.gid).name
+    mode "0644"
+  end
+end
+
