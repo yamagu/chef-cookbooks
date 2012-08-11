8db3ea65f0600742b0b3cbb7542fb11c92818972
yamagu <www.yamagu@gmail.com>
2012-08-12 00:48:03 +0900

    php remi

--- php/recipes/default.rb
+++ php/recipes/default.rb
@@ -8,10 +8,19 @@
 #
 include_recipe "yum::remi"
 
+execute "remi_enabled" do
+  command "perl -p -i.bak -e 's/enabled=0/enabled=1/' /etc/yum.repos.d/remi.repo"
+end
+
+
 node['php']['packages'].each do |name|
   yum_package name do
     version node['php']['version']
     action :install
     options "--enablerepo=remi"
   end
+end
+
+execute "remi_disabled" do
+  command "perl -p -i.bak -e 's/enabled=1/enabled=0/' /etc/yum.repos.d/remi.repo"
 end
