a96373d40d164b83e3fe6f16bceae009ff068a28
yamagu <www.yamagu@gmail.com>
2013-03-27 12:41:55 +0900

    postfix vmailbox

--- postfix/attributes/default.rb
+++ postfix/attributes/default.rb
@@ -7,3 +7,14 @@ default['postfix']['mail_relay_networks']        = "127.0.0.0/8"
 default['postfix']['relayhost_role']             = "relayhost"
 default['postfix']['multi_environment_relay'] = false
 default['postfix']['inet_interfaces'] = nil
+default['postfix']['virtual_mailbox'] = {
+  "enabled" => false,
+  "virtual_mailbox_maps" => "hash:/etc/postfix/vmailbox",
+  "virtual_uid" => 5000,
+  "virtual_gid" => 5000,
+  "virtual_mailbox_domains" => "example.com"
+}
+default['postfix']['virtual_alias_maps'] = {
+  "file" => "hash:/etc/postfix/virtual",
+  "entry" => {}
+}
